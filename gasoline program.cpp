2037fe075de51415f95e247a0eae90ab29f58f4b
shreyansh-tyagi <sunnytyagi886@gmail.com>
2020-12-26 18:27:51 +0530

    question of gasoline program added

--- gasoline program.cpp
+++ gasoline program.cpp
@@ -1,3 +1,41 @@
+/*Let suppose N cars are parked on a circular track**(1 to N)** at distance of 1km each. Each car contains some fuels (Fi to Fn).A car runs 2km per litre of fuel. You are driving car 1 in the clockwise direction. To move one unit of distance in this direction, you need to spend half litre of gasoline. When you pass another car parked at even position (even if you'd run out of gasoline exactly at that point), you steal all its gasoline and When you pass another car parked at odd position,you steal half of its gasoline each time. Once you do not have any gasoline left, you stop.
+
+Here is one catch that you will not steal gasoline from other cars if that car has less then 2 litres of gasoline.
+
+What is the total clockwise distance travelled by your car?
+
+Input Format
+
+The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
+
+The first line of each test case contains a single integer N.
+
+The second line contains N space-separated integers f1,f2,…,fN.
+
+Constraints
+
+1≤ T ≤100
+
+1≤ N ≤100
+
+0≤ fi ≤100
+
+Output Format
+
+Total distance traveled for each test cases into seperated line.
+
+Sample Input 0
+
+2
+5
+1 2 3 4 5
+6
+2 5 2 0 1 0
+Sample Output 0
+
+ 24.5
+ 16 */
+
 #include<bits/stdc++.h>
 using namespace std;
 
