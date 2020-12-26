488446e72603c3f85cb0f54be46214f99cb10d12
shreyansh-tyagi <sunnytyagi886@gmail.com>
2020-12-26 18:29:38 +0530

    added question of converstion of javascript variable to c++

--- coverstion javascirpt variable to c++.cpp
+++ coverstion javascirpt variable to c++.cpp
@@ -1,3 +1,43 @@
+/*Problem
+Submissions
+Leaderboard
+Discussions
+Mr. RAM developed a logic in JavaScript but client requested him to channge his logic in C++, so he decided to write a logic to Convert a JavaScript variable into C++ variable as per specified condition(s),
+
+C++ variables allow underscore whereas Javascript variables do not allow underscore. if a variable in Javascript is defined as- myJavascriptVariable convert into C++ (insert _ before any capital letter and convert the capital letter into small letter) my_javascript_variable
+
+Input Format
+
+one line which takes input from user as javascript variable and convert it into C++ variable as specified.
+
+Constraints
+
+User must enter a variable which contains a capital letter in it.
+
+Output Format
+
+Add underscore before capital letter (JavaScript variable) and all letters in variable name must be in small letters.
+
+Sample Input 0
+
+myJavascriptVariable
+Sample Output 0
+
+my_javascript_variable
+Explanation 0
+
+There are two capital letters so, underscore is added before both and capital letters are converted to small
+Contest ends in 3 days
+Submissions: 8
+Max Score: 10
+Difficulty: Medium
+Rate This Challenge:
+
+    
+More
+ 
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
