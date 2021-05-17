ff1b4459dd2f564356165883b100bc3ee0bd5f90
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-05-17 23:16:56 +0530

    rotate string

---
+++ rotate string.c
@@ -0,0 +1,33 @@
+/*
+Karan got a task to left rotate a string by one place. You need to help him by designing an algorithm that gives the second letter of rotated string
+
+Input Format
+
+Enter the string of size ‘n’.
+
+Constraints
+
+1<= n <= 20
+
+Output Format
+
+Print the letter.
+
+Sample Input 0
+
+aryan
+Sample Output 0
+
+y
+*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char ch[30];
+    scanf("%s",ch);
+    printf("%c",ch[2]);
+}
