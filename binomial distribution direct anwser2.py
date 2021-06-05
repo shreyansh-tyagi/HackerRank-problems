70294897a16f95336fed3437fb67f1de2035cf25
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-06-05 21:50:46 +0530

    binomial distribution has been added

---
+++ binomial distribution direct anwser2.py
@@ -0,0 +1,38 @@
+'''
+
+In this challenge, we go further with binomial distributions. We recommend reviewing the previous challenge's Tutorial before attempting this problem.
+
+Task
+A manufacturer of metal pistons finds that, on average,  of the pistons they manufacture are rejected because they are incorrectly sized. What is the probability that a batch of  pistons will contain:
+
+No more than  rejects?
+At least  rejects?
+Input Format
+
+A single line containing the following values (denoting the respective percentage of defective pistons and the size of the current batch of pistons):
+
+12 10
+If you do not wish to read this information from stdin, you can hard-code it into your program.
+
+Output Format
+
+Print the answer to each question on its own line:
+
+The first line should contain the probability that a batch of  pistons will contain no more than  rejects.
+The second line should contain the probability that a batch of  pistons will contain at least  rejects.
+Round both of your answers to a scale of  decimal places (i.e.,  format).
+
+'''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def fact(n):
+    return 1 if n == 0 else n*fact(n-1)
+
+def comb(n, x):
+    return fact(n) / (fact(x) * fact(n-x))
+
+def b(x, n, p):
+    return comb(n, x) * p**x * (1-p)**(n-x)
+
+p, n = list(map(int, input().split(" ")))
+print(round(sum([b(i, n, p/100) for i in range(3)]), 3))
+print(round(sum([b(i, n, p/100) for i in range(2, n+1)]), 3))
