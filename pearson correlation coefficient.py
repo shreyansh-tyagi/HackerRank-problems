80f9c14ed4d44256ad8e61fc312c664bfd7a2228
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-06-17 01:45:24 +0530

    pearson correlation coefficient.py

---
+++ pearson correlation coefficient.py
@@ -0,0 +1,53 @@
+'''
+In this challenge, we practice calculating the Pearson correlation coefficient. Check out the Tutorial tab for learning materials!
+
+Task
+Given two -element data sets,  and , calculate the value of the Pearson correlation coefficient.
+
+Input Format
+
+The first line contains an integer, , denoting the size of data sets  and .
+The second line contains  space-separated real numbers (scaled to at most one decimal place), defining data set .
+The third line contains  space-separated real numbers (scaled to at most one decimal place), defining data set .
+
+Constraints
+
+, where  is the  value of data set .
+, where  is the  value of data set .
+Data set  contains unique values.
+Data set  contains unique values.
+Output Format
+
+Print the value of the Pearson correlation coefficient, rounded to a scale of  decimal places.
+
+Sample Input
+
+10
+10 9.8 8 7.8 7.7 7 6 5 4 2 
+200 44 32 24 22 17 15 12 8 4
+Sample Output
+
+0.612
+Explanation
+
+The mean and standard deviation of data set  are:
+
+The mean and standard deviation of data set  are:
+
+We use the following formula to calculate the Pearson correlation coefficient:
+
+
+'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import statistics as st
+a=int(input())
+x=list(map(float,input().split()))[:a]
+y=list(map(float,input().split()))[:a]
+b=st.mean(x)
+c=st.stdev(x)
+d=st.mean(y)
+e=st.stdev(y)
+f=sum([(x[i]-b)*(y[i]-d) for i in range(a)])
+g=a*c*e
+print(f/g)
